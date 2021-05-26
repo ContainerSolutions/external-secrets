86f266e896a56e9390bc3d490ecbb70ccc367ca5
Sebastian Gomez <sebastiangomezcorrea@gmail.com>
2021-05-26 10:12:25 -0400

    More stuff

--- pkg/provider/ibm/provider.go
+++ pkg/provider/ibm/provider.go
@@ -5,41 +5,31 @@ import (
 
 	"github.com/IBM/go-sdk-core/v5/core"
 	sm "github.com/IBM/secrets-manager-go-sdk/secretsmanagerv1"
-	"github.com/external-secrets/external-secrets/pkg/provider/aws/session"
 	"github.com/go-logr/logr"
 )
+
 
 type provider struct {
 }
 
-func (p *provider) NewClient(ctx context.Context, store esv1alpha1.GenericStore, kube kclient.Client, namespace string) (provider.SecretsClient, error) {
-	storeSpec := store.GetSpec()
-	imbSpec := storeSpec.Provider.IBM
+const (
+	SecretsManagerEndpointEnv = "IBM_SECRETSMANAGER_ENDPOINT"
+	STSEndpointEnv            = "IBM_STS_ENDPOINT"
+	SSMEndpointEnv            = "IBM_SSM_ENDPOINT"
 
-	secretsManager, err := sm.NewSecretsManagerV1(&sm.SecretsManagerV1Options{
-		URL: "<SERVICE_URL>",
-		Authenticator: &core.IamAuthenticator{
-			ApiKey: "<IBM_CLOUD_API_KEY>",
-		},
-	})
-	if err != nil {
-		panic(err)
-	}
-
-	iStore := &client{
-		kube:      kube,
-		store:     ibmSpec,
-		log:       ctrl.Log.WithName("provider").WithName("ibm"),
-		namespace: namespace,
-		storeKind: store.GetObjectKind().GroupVersionKind().Kind,
-	}
-
-	cfg := bluemix.Config{
-		//	bluemix.Config.IAMAccessToken:
-	}
-
-	session = session.New(&cfg)
-}
+	errUnableCreateSession                     = "unable to create session: %w"
+	errUnknownProviderService                  = "unknown IBM Provider Service: %s"
+	errInvalidClusterStoreMissingAKIDNamespace = "invalid ClusterSecretStore: missing IBM AccessKeyID Namespace"
+	errInvalidClusterStoreMissingSAKNamespace  = "invalid ClusterSecretStore: missing IBM SecretAccessKey Namespace"
+	errFetchAKIDSecret                         = "could not fetch accessKeyID secret: %w"
+	errFetchSAKSecret                          = "could not fetch SecretAccessKey secret: %w"
+	errMissingSAK                              = "missing SecretAccessKey"
+	errMissingAKID                             = "missing AccessKeyID"
+	errNilStore                                = "found nil store"
+	errMissingStoreSpec                        = "store is missing spec"
+	errMissingProvider                         = "storeSpec is missing provider"
+	errInvalidProvider                         = "invalid provider spec. Missing IBM field in store %s"
+)
 
 type client struct {
 	kube      kclient.Client
@@ -57,4 +47,31 @@ func (ibm *client) GetSecret(ctx context.Context, ref esv1alpha1.ExternalSecretD
 
 func (ibm *client) GetSecretMap(ctx context.Context, ref esv1alpha1.ExternalSecretDataRemoteRef) (map[string][]byte, error) {
 
+}
+
+func (p *provider) NewClient(ctx context.Context, store esv1alpha1.GenericStore, kube kclient.Client, namespace string) (provider.SecretsClient, error) {
+	storeSpec := store.GetSpec()
+	imbSpec := storeSpec.Provider.IBM
+
+	secretsManager, err := sm.NewSecretsManagerV1(&sm.SecretsManagerV1Options{
+		URL: "<SERVICE_URL>",
+		Authenticator: &core.IamAuthenticator{
+			ApiKey: "<IBM_CLOUD_API_KEY>",
+		},
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	iStore := &client{
+		client:    secretsManager,
+		kube:      kube,
+		store:     ibmSpec,
+		log:       ctrl.Log.WithName("provider").WithName("ibm"),
+		namespace: namespace,
+		storeKind: store.GetObjectKind().GroupVersionKind().Kind,
+	}
+
+	return iStore, nil
 }
